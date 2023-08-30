117b6d0093fc3f34561ccc91cf4557ef15926396
Luciano1010 <luciano_lopesdealmeida@hotmail.com>
2023-08-30 15:01:09 -0300

    Menu

---
+++ Menu/Program.cs
@@ -0,0 +1,54 @@
+﻿namespace Menu
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int opção;
+            
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.White;
+            //while(true) 
+            Console.WriteLine("------------------------------------------------------------     ");
+            Console.WriteLine(" *****                                                            ");
+            Console.WriteLine("        Seja Bem Vindo - Brazilem  Express                        ");
+            Console.WriteLine("                                                                  ");
+            Console.WriteLine("------------------------------------------------------------      ");                  
+            Console.WriteLine(" Digite a Opção desejada:                                         ");
+            Console.WriteLine("                                                                  ");
+            Console.WriteLine("                      1 - Criar Conta                             ");
+            Console.WriteLine("                      2 - Listar todas as Contas                  ");
+            Console.WriteLine("                      3 - Buscar Conta por Numero                 ");
+            Console.WriteLine("                      4 - Atualizar Dados da conta                ");
+            Console.WriteLine("                      5 - Apagar Conta                            ");
+            Console.WriteLine("                      6 - Sacar                                   ");
+            Console.WriteLine("                      7 - Depositar                               ");
+            Console.WriteLine("                      8 - Transferir valores entre Contas         ");
+            Console.WriteLine("                      9 - Sair                                    ");
+            Console.WriteLine("                                                                  ");
+            Console.WriteLine("----------------------------------------------------------------  ");
+            Console.ResetColor();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
